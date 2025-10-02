66b12384490aa17b4efe0e135a5287c7678a1bca
cnmzsjbz199328 <tj15982183241@outlook.com>
2025-10-02 19:52:05 +0930

    refactor: Update font link in RootLayout to utilize new Material Symbols Outlined font for improved iconography and visual consistency across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
     <html lang="en-US">
       <head>
         <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" 
         />
       </head>
       <body
